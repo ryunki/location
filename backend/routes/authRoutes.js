4bab78c3583db8ae13b0c57b9f9ec34ae4ec46aa
ryunkishin <ryunkishin@gmail.com>
2024-02-15 20:51:35 +0900

    installations for backend

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router()
+
+const controllers = require('./controllers/auth/authControllers')
+
+
+router.post('/login',controllers.postLogin)
+router.post('/register',controllers.postRegister)
